7e79c97e70e30a9e1e241b6717c4dcdc9f6868ea
Tanner Ball <tanner@lexingtongroup.co>
2017-08-13 18:10:42 -0400

    adds charts and begins notes

--- src/components/bank/StudentProfile.vue
+++ src/components/bank/StudentProfile.vue
@@ -9,32 +9,7 @@
 				<deposits v-if="student.id" :student="student"></deposits>
 				<missing-work-list v-if="student.id" :student="student"></missing-work-list>
 				<tier-two-report v-if="student.id && student.is_ttwo" :student="student"></tier-two-report>
-				<div v-if="student.is_tthree" class="x_panel">
-					<div class="x_title">
-						<h2>PASS <small>Recent reports for {{student.first_name}} {{student.last_name}}</small></h2>
-						<div class="clearfix"></div>
-					</div>
-					<div class="x_content">
-						<table class="table table-condensed">
-							<thead>
-								<th>Date</th>
-								<th>Course</th>
-								<th>Score</th>
-							</thead>
-							<tbody>
-								<tr v-for="report in tthree_reports">
-									<td style="text-align:left;">{{report.report.date}}</td>
-									<td style="text-align:left;">{{report.report.course.name}}</td>
-									<td v-if="report.score==1" style="text-align:left;background-color:red;">Red</td>
-									<td v-if="report.score==2" style="text-align:left;background-color:yellow;">Yellow</td>
-									<td v-if="report.score==3" style="text-align:left;background-color:green;">Green</td>
-									<td v-if="report.score==4" style="text-align:left;background-color:blue;">Blue</td>
-								</tr>
-							</tbody>
-						</table>
-						<div class="clearfix"></div>
-					</div>
-				</div>
+				<tier-three-report v-if="student.id && student.is_tthree" :student="student"></tier-three-report>
 			</div>
 		</div>
 	</div>
@@ -48,19 +23,16 @@ import StudentScheduleComponent from "./StudentProfile/StudentSchedule.vue"
 import StudentStatisticsComponent from "./StudentProfile/StudentStatistics.vue"
 import DepositsComponent from "./StudentProfile/Deposits.vue"
 import TierTwoReportComponent from "./StudentProfile/TierTwoReport.vue"
+import TierThreeReportComponent from "./StudentProfile/TierThreeReport.vue"
 
 export default {
 	name: 'StudentProfileComponent',
 	data: function(){
 		return {
 			student: {},
-			ttwo_reports: {},
-			tthree_reports: {},
 		}
 	},
-	props: {
-		user: {},
-	},
+	props: ['user'],
 	components: {
 		'modal': BasicModalComponent,
 		'MissingWorkList':MissingWorkListComponent,
@@ -69,15 +41,11 @@ export default {
 		'StudentStatistics':StudentStatisticsComponent,
 		'Deposits':DepositsComponent,
 		'TierTwoReport':TierTwoReportComponent,
+		'TierThreeReport':TierThreeReportComponent,
 	},
 	created: function(){
 		var self = this;
 		self.fetchStudent();
-	},
-	mounted: function(){
-		var self = this;
-		self.fetchTTwoReports();
-//		self.fetchTThreeReports();
 	},
 	methods: {
 		fetchStudent: function(){
@@ -87,26 +55,6 @@ export default {
 			.then(function(response){
 				self.student = response.data;
 			});
-		},
-		fetchTTwoReports: function(){
-			var self = this;
-			var url = '/bank/student/ttwo_chart/';
-			var data = {
-				'student_id':self.$route.params.student_id,
-			}
-			console.log(data);
-			self.$http.post(url,data)
-			.then(function(response){
-				console.log(response);
-			})
-		},
-		fetchTThreeReports: function(){
-			var self = this;
-			var url = '/bank/student/recent_tthree_reports/'+self.$route.params.student_id+'/';
-			self.$http.get(url)
-			.then(function(response){
-				self.tthree_reports = response.data;
-			})
 		},
 	}
 }
--- src/components/bank/StudentProfile/TierTwoReport.vue
+++ src/components/bank/StudentProfile/TierTwoReport.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="x_panel" v-if="charts.length > 0">
+	<div class="x_panel cac_panel" v-if="charts.length > 0">
 		<div class="x_title">
 			<h2>Check and Connect <small>Recent reports for {{student.first_name}} {{student.last_name}}</small></h2>
 			<div class="clearfix"></div>
 		</div>
 		<div class="x_content">
 			<div v-for="chart in charts">
-				<h4>{{chart.goal.name}}</h4>
+				<h4>{{chart.goal.goal}}</h4>
 				<table class="table table-condensed">
 					<thead>
 						<th v-for="header in chart.col_headers">{{header}}</th>
-						<th>4/5</th>
+						<th>4 and 5</th>
 					</thead>
 					<tbody>
 						<tr v-for="row in chart.courses">
@@ -18,18 +18,35 @@
 							<td v-for="score in row.scores">{{score}}</td>
 							<td>{{row.summary}}</td>
 						</tr>
+					</tbody>
+					<tfoot>
 						<tr>
-							<td>4/5</td>
+							<td style="text-align:left;">4 and 5</td>
 							<td v-for="score in chart.totals.scores">{{score}}</td>
 							<td>{{chart.totals.summary}}</td>
-						</tr>
-					</tbody>
+						</tr>	
+					</tfoot>
 				</table>
 			</div>
 			<div class="clearfix"></div>
 		</div>
 	</div>
 </template>
+
+<style>
+	div.cac_panel tfoot tr td {
+		font-weight: bold;
+	}
+	div.cac_panel tbody tr td:last-child {
+		font-weight: bold;
+	}
+	div.cac_panel tbody tr td:first-child {
+		text-align: left;
+	}
+	div.cac_panel tfoot tr td:first-child {
+		text-align: left;
+	}
+</style>
 
 <script>
 export default {
@@ -38,10 +55,12 @@ export default {
 	data: function(){
 		return {
 			charts: [],
+			notes: []
 		}
 	},
 	created: function(){
-		this.fetchTTwoReports();	
+		this.fetchTTwoReports();
+		this.fetchTTwoNotes();
 	},
 	methods: {
 		fetchTTwoReports: function(){
@@ -50,12 +69,22 @@ export default {
 			var data = {
 				'student_id':self.student.id,
 			}
-			console.log(data);
 			self.$http.post(url,data)
 			.then(function(response){
 				self.charts = response.data;
 			})
 		},
+		fetchTTwoNotes: function(){
+			var self = this;
+			var url = '/bank/student/ttwo_notes/';
+			var data = {
+				'student_id':self.student.id,
+			}
+			self.$http.post(url,data)
+			.then(function(response){
+				self.notes = response.data;
+			})
+		}
 	}
 }
 </script>
