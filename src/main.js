f5a89a8cfaa0772a8f5e593f7a9feca43f8eb98f
nikolai0045 <tanner.ball@campusoutreach.org>
2017-08-17 12:25:03 -0400

    fast fix!

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import VueCookie from 'vue-cookie'
 
 const BANK_SETTINGS = {
-  api_url:'http://localhost:8000/',
+  api_url:'http://api.buffalo-bank.com/',
 }
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
